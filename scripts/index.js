351fc4969fa41271538f3e17dc4624766407bd50
DKSolo <dbm95mail@gmail.com>
2024-02-22 19:36:40 +0500

    First Upload: Initial Cards Creation and Delete button Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,71 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const contentContainer = document.querySelector('.content');
+const placesListContainer = contentContainer.querySelector('.places__list');
+const addButton = contentContainer.querySelector('.profile__add-button');
+const resetButton = contentContainer.querySelector('.input__btn_action_reset');
+const noSongsElement = contentContainer.querySelector('.no-songs');
+
+
+
+//* Здесь я создаю функцию, которая считывает массив и затем элемент за элементом массива 
+//* Отображает карточки.
+
+function initialSetUp() {
+    console.log('Начинаю первоначальную загрузку карточек...')
+    for (let i = 0; i < initialCards.length; i++) {
+        let cardsName = initialCards[i].name;
+        let cardsLink = initialCards[i].link;
+        addCardwTitleNLink(cardsName, cardsLink);
+    }
+    console.log('Загрузка карточек завершена!')
+
+
+}
+
+function addCardwTitleNLink(cardTitle, cardLink) {
+    console.log('Добавляю: '+ cardTitle);
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+     
+
+    cardElement.querySelector('.card__image').setAttribute('src', cardLink);
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+        //console.log(evt.target);
+        console.log('Кнопка удаления нажата на ' + cardTitle);
+        deleteCardwTitleNLink (cardTitle, cardLink)
+    });
+    //const deleteCardBtn = cardElement.querySelector('.card__delete-button');
+    //deleteCardBtn.addEventListener('click',  deleteCardwTitleNLink(cardTitle, cardLink));
+    placesListContainer.append(cardElement);
+}
+
+function deleteCardwTitleNLink (cardTitle, cardLink) {
+    console.log('Ищу: ' + cardTitle);
+    
+    listOfExistingCards = placesListContainer.querySelectorAll('.card');
+
+    for (let ii = 0; ii < listOfExistingCards.length; ii++) {
+        const cardElementToDelete = listOfExistingCards[ii];
+        if (cardTitle === listOfExistingCards[ii].querySelector('.card__title').textContent) {
+            console.log('Удаляю: ' + cardTitle);
+            listOfExistingCards[ii].remove();
+        }
+        else {
+            console.log('Кнопка нажата, ничего не удалено!')
+        }
+        
+    }
+}
+
+//* Исполенение функций
+
+initialSetUp()
+
+//* Поиск кнопок удаления 
+
+
