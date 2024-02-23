614a341b66768c26964d5abf44b31a582709ef86
DKSolo <dbm95mail@gmail.com>
2024-02-23 17:56:04 +0500

    Second Upload: Optimised Delete Funct, Create Function No longer appends to DOM but simply returns element, card images have alts now.

--- scripts/index.js
+++ scripts/index.js
@@ -11,78 +11,47 @@
 
 const contentContainer = document.querySelector('.content');
 const placesListContainer = contentContainer.querySelector('.places__list');
-const addButton = contentContainer.querySelector('.profile__add-button');
-const resetButton = contentContainer.querySelector('.input__btn_action_reset');
-const noSongsElement = contentContainer.querySelector('.no-songs');
-
-
 
 //* Здесь я создаю функцию, которая считывает массив и затем элемент за элементом массива 
 //* Отображает карточки.
 
 function initialSetUp() {
+    //Функция начала загрузки карточик
     console.log('Начинаю первоначальную загрузку карточек...')
     for (let i = 0; i < initialCards.length; i++) {
-        let cardsName = initialCards[i].name;
-        let cardsLink = initialCards[i].link;
-        addCardwTitleNLink(cardsName, cardsLink);
+        const cardsName = initialCards[i].name;
+        const cardsLink = initialCards[i].link;
+        const cardElement = createCardwTitleNLink(cardsName, cardsLink);
+        placesListContainer.append(cardElement);
     }
     console.log('Загрузка карточек завершена!')
 
 
 }
 
-function addCardwTitleNLink(cardTitle, cardLink) {
+function createCardwTitleNLink(cardTitle, cardLink) {
+    //Фнукция добавления карточки по имени и ссылке
     console.log('Добавляю: '+ cardTitle);
     const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-     
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);     
 
     cardElement.querySelector('.card__image').setAttribute('src', cardLink);
+    cardElement.querySelector('.card__image').setAttribute('alt', 'Красивая картинка карточки. На картинке - ' + cardTitle);
     cardElement.querySelector('.card__title').textContent = cardTitle;
     cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
-        //console.log(evt.target);
         console.log('Кнопка удаления нажата на ' + cardTitle);
-        deleteCardwTitleNLink (cardTitle, cardLink)
+        deleteCard(evt);
     });
-    //const deleteCardBtn = cardElement.querySelector('.card__delete-button');
-    //deleteCardBtn.addEventListener('click',  deleteCardwTitleNLink(cardTitle, cardLink));
-    placesListContainer.append(cardElement);
-
     return cardElement;
 }
 
-function deleteCardwTitleNLink (cardTitle, cardLink) {
-    console.log('Ищу: ' + cardTitle);
-    let errFlag = 0
-    
-    listOfExistingCards = placesListContainer.querySelectorAll('.card');
-
-    for (let ii = 0; ii < listOfExistingCards.length; ii++) {
-        const cardElementToDelete = listOfExistingCards[ii];
-        if (cardTitle === listOfExistingCards[ii].querySelector('.card__title').textContent) {
-            console.log('Удаляю: ' + cardTitle);
-            listOfExistingCards[ii].remove();
-            errFlag += 1;
-            break;
-        }
-        else if ((errFlag === 0) && (ii === (listOfExistingCards.length-1))) {
-            console.log('ОШИБКА! Кнопка нажата, ничего не удалено!');
-        }
-        else if ((errFlag === 1) && (ii === (listOfExistingCards.length-1))) {
-            console.log('ОШИБКА! Карточка успешно удалена! Однако цикл продолжается. errFlag = ' + errFlag);
-        }
-        else {
-            console.log('Кнопка нажата. Ищу...');
-        }
-        
-    }
+function deleteCard (evt) {
+    //функция удаления карточки
+    evt.target.closest('.card').remove()
 }
 
 //* Исполенение функций
 
 initialSetUp()
-
-//* Поиск кнопок удаления 
 
 
